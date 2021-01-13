af97a75bb808eaba3ce9b00e2fc2d85f6d6d1bac
JunsooLee <junsoolee65@naver.com>
2021-01-13 22:01:41 +0900

    타코 재료 선택 및 주문 페이지
    타코 재료 선택 및 주문 페이지

--- taco-cloud/src/main/java/tacos/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/DesignTacoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Taco;
@@ -37,7 +38,14 @@ public class DesignTacoController {
 
 		return "design";
 	}
-
+	
+	@PostMapping
+	public String processDesign(Taco design) {
+		log.info("Processing design: " + design);
+		
+		return "redirect:/orders/current";
+	}
+	
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
 		return ingredients
 				.stream()
